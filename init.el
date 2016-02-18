512d076f4d206a7730ccc23919739e4442fc0dc6
ygilad <yanivdll@gmail.com>
2016-02-18 09:03:50 -0500

    Switched to work with .authinfo.gpg

--- init.el
+++ init.el
@@ -12,7 +12,9 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/org-mode/lisp")
 (add-to-list 'load-path "~/.emacs.d/lisp/org-reveal/")
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(setq load-path (cons "~/.emacs.d/org2blog/" load-path))
+(setq load-path (cons "~/.emacs.d/lisp/org2blog/" load-path))
+
+
 (require 'org2blog-autoloads)
 ;; Load the rest of the packages
  (package-initialize nil)
@@ -20,24 +22,28 @@
 (require 'package)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  ;;'(custom-enabled-themes (quote (greymatters)))
-  '(custom-safe-themes t)
-  '(custom-theme-directory "~/.emacs.d/themes/")
-  '(default-frame-alist '((height . 70) (width . 120) (tool-bar-lines . 0) (background-color . "honeydew")))
-  '(desktop-save-mode t)
-  '(global-visual-line-mode t)
-  '(global-hl-line-mode 1) ; turn on highlighting current line
-  '(show-paren-mode 1)
-  '(markdown-command "/usr/local/bin/multimarkdown")
-  '(markdown-open-command "/Applications/Safari.app/Contents/MacOS/Safari")
-  '(package-archives
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes t)
+ '(custom-theme-directory "~/.emacs.d/themes/")
+ '(default-frame-alist
     (quote
-     (("gnu" . "http://elpa.gnu.org/packages/")
-      ("marmalade" . "http://marmalade-repo.org/packages/")
-      ("melpa" . "https://melpa.org/packages/")))))
+     ((height . 70)
+      (width . 120)
+      (tool-bar-lines . 0))))
+ '(desktop-save-mode t)
+ '(global-hl-line-mode 1)
+ '(global-visual-line-mode t)
+ '(markdown-command "/usr/local/bin/multimarkdown")
+ '(markdown-open-command "/Applications/Safari.app/Contents/MacOS/Safari")
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("marmalade" . "http://marmalade-repo.org/packages/")
+     ("melpa" . "https://melpa.org/packages/"))))
+ '(send-mail-function (quote smtpmail-send-it))
+ '(show-paren-mode 1))
 
 (package-initialize)
 
