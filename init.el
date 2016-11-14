638e11652e8976e92cba7f2a033cd5ee8b6f1ba3
ygilad <yanivdll@gmail.com>
2016-11-13 21:36:02 -0500

    Deactivated the spacemacs theme. Back to Emacs defalt.

--- init.el
+++ init.el
@@ -33,8 +33,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-  ;; (setq-default fill-column 150)
-  (scroll-bar-mode -1)
+ ;; (setq-default fill-column 120)
+ (scroll-bar-mode -1)
  '(custom-safe-themes t)
  '(custom-theme-directory "~/.emacs.d/themes/")
  '(default-frame-alist (quote ((height . 80) (width . 70) (tool-bar-lines . 0))))
